32684bbbee1f5462c790c8552a1fb7543541dee7
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-02-06 11:57:07 +0530

    Error handling

--- src/App.js
+++ src/App.js
@@ -6,22 +6,33 @@ import { useState } from "react";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [isLoading,setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const getMoviesHandler = async () => {
-    setIsLoading(true)
-    const response = await fetch("https://swapi.dev/api/films/");
-    const data = await response.json();
+    try {
+      setIsLoading(true);
+      setError(null);
+      const response = await fetch("https://swapi.dev/api/film/");
 
-    const transformedMovies = data.results.map((m) => {
-      return {
-        id: m.episode_id,
-        title: m.title,
-        releaseDate: m.release_date,
-        openingText: m.opening_crawl,
-      };
-    });
-    setMovies(transformedMovies);
+      if (!response.ok) {
+        throw new Error("something went wrong Retrying...");
+      }
+
+      const data = await response.json();
+
+      const transformedMovies = data.results.map((m) => {
+        return {
+          id: m.episode_id,
+          title: m.title,
+          releaseDate: m.release_date,
+          openingText: m.opening_crawl,
+        };
+      });
+      setMovies(transformedMovies);
+    } catch (error) {
+      setError(error.message);
+    }
     setIsLoading(false);
   };
 
@@ -31,17 +42,20 @@ function App() {
         <button onClick={getMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        {!isLoading && <MoviesList movies={movies}/>}
-        {isLoading && <Button variant="primary" disabled>
-        <Spinner
-          as="span"
-          animation="border"
-          size="sm"
-          role="status"
-          aria-hidden="true"
-        />
-        <span className="visually-hidden">Loading...</span>
-      </Button>}
+        {!isLoading && <MoviesList movies={movies} />}
+        {isLoading && (
+          <Button variant="primary" disabled>
+            <Spinner
+              as="span"
+              animation="border"
+              size="sm"
+              role="status"
+              aria-hidden="true"
+            />
+            <span className="visually-hidden">Loading...</span>
+          </Button>
+        )}
+        {!isLoading && error && <><p>{error}</p><Button>Cancel</Button></>}
       </section>
     </React.Fragment>
   );
