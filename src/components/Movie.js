3b214303e53b1722cee2bd9bbb330508dcebba1b
Pratiksha9240 <dhavalepratiksha9240@gmail.com>
2023-02-06 17:50:19 +0530

    delete movie functionality added

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 
 import classes from './Movie.module.css';
 
 const Movie = (props) => {
+
+  const deleteMovieHandler = (id) => {
+    const response = fetch('https://react-http-b29b9-default-rtdb.firebaseio.com/movies.json', {
+      method: 'DELETE',
+    body: id})
+    var d = document.getElementById(id);
+    d.remove();
+    
+  }
+
   return (
-    <li className={classes.movie}>
+    <li className={classes.movie} id={props.id}>
       <h2>{props.title}</h2>
       <h3>{props.releaseDate}</h3>
       <p>{props.openingText}</p>
+      <Button variant='danger' onClick={() => deleteMovieHandler(props.id)}>Delete</Button>
     </li>
   );
 };
